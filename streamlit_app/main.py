279054ed9d09707cf42ae0a6708edf7a9eeaeea7
Dax Ledesma <37383659+dakilaledesma@users.noreply.github.com>
2024-02-14 18:55:57 -0500

    Initial streamlit implementation and refactor library paths

--- streamlit_app/main.py
+++ streamlit_app/main.py
@@ -1,12 +1,15 @@
+import streamlit as st
 import os
-from MusicLM.MusicLM import Music
+from streamlit_app.MusicLM.MusicLM import Music
 
+st.title("QM/MAI MVP")
 # Soft-fix given dotenv not working
 os.environ["TOKEN"] = ""
 
-music = Music()
-input = "Ambient, soft sounding music I can study to"
-tracks = music.get_tracks(input, 2)
+if st.button("Generate"):
+    music = Music()
+    input = "Ambient, soft sounding music I can study to"
+    tracks = music.get_tracks(input, 2)
 
-if isinstance(tracks, list):
-    music.b64toMP3(tracks, input)
+    if isinstance(tracks, list):
+        music.b64toMP3(tracks, input)
